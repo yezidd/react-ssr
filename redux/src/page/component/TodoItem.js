8736e71c58bf646e17b647ed3558ef22f9160d01
yzdd <yjs10086>
2018-03-26 16:35:32 +0800

    todolist功能更新完毕，准备加入 拖拽和修改功能

--- redux/src/page/component/TodoItem.js
+++ redux/src/page/component/TodoItem.js
@@ -2,21 +2,37 @@
  * Created by yzdd on 2018/3/21.
  */
 import React, {Component} from 'react';
+import {findDomNode} from 'react-dom';
 import './todoItem.css'
 import {connect} from "react-redux";
-import {completeTodo, deleteTodo} from "../../action/todo";
+import {completeTodo, deleteTodo, modifyTodo} from "../../action/todo";
 
 class TodoItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      checkValue: this.props.item.complete
+      checkValue: this.props.item.complete,
+      status: "text",
+      textValue: this.props.item.text
     }
   }
 
+  handleBlur = () => {
+    if (this.state.textValue !== "") {
+      this.props.modifyTodoBtn({index: this.props.item.index, text: this.state.textValue});
+      this.setState({
+        status: "text"
+      });
+    } else {
+      this.setState({
+        status: "text"
+      })
+    }
+  };
+
   render() {
     const {style, check, complete, item, deleteBtn} = this.props;
-    console.log(item.index, item,this.props.item.complete, this.state.checkValue, "----");
+    console.log(item.index, item, this.props.item.complete, this.state.checkValue, "----");
     return (
       <li className="item" style={style}>
         <input
@@ -31,7 +47,21 @@ class TodoItem extends Component {
           }}
           checked={this.state.checkValue}
         />
-        <p className="content">{item.text}</p>
+        {
+          this.state.status === "change" ?
+            <input
+              onChange={(e) => this.setState({textValue: e.target.value})}
+              value={this.state.textValue}
+              onBlur={() => this.handleBlur()}
+              autoFocus
+              style={{flex: 1, marginRight: 100}}
+              ref="myInput"
+              id="myCheck"
+              onFocus={() => document.getElementById("myCheck").setSelectionRange(0, this.state.textValue.length)}
+            />
+            : <p className="content" onClick={() => this.setState({status: "change"})}>{item.text}</p>
+        }
+
         <a className="deleteBtn" onClick={() => deleteBtn(item.index)}>-</a>
       </li>
     );
@@ -45,7 +75,8 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   return {
     complete: (index) => dispatch(completeTodo(index)),
-    deleteBtn: (index) => dispatch(deleteTodo(index))
+    deleteBtn: (index) => dispatch(deleteTodo(index)),
+    modifyTodoBtn: ({index, text}) => dispatch(modifyTodo({index, text}))
   }
 }
 
